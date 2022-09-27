e18250c816415fdce586a4aae5ba0783a92734ec
Ol4ik88 <slapikol4ik@gmail.com>
2022-09-27 15:51:17 +0300

    feat: add data file for cards

---
+++ src/data/data.ts
@@ -0,0 +1,46 @@
+const cards = [
+  {
+    id: 1,
+    title: 'Cat',
+    img: 'https://www.belanta.vet/vet-blog/wp-content/uploads/2019/09/5-4.jpg',
+    description: 'text',
+  },
+  {
+    id: 2,
+    title: 'Big cat',
+    img: 'https://blitzpet.ru/wp-content/uploads/2019/04/kotyonok-ploho-est-suhoj-korm-v-chyom-prichina.jpg',
+    description: 'text',
+  },
+  {
+    id: 3,
+    title: 'Dog',
+    img: 'https://mobimg.b-cdn.net/v3/fetch/0e/0e26b1b65946ee36fac9605ae67e4ac8.jpeg',
+    description: 'text',
+  },
+  {
+    id: 4,
+    title: 'Cat',
+    img: 'https://www.belanta.vet/vet-blog/wp-content/uploads/2019/09/5-4.jpg',
+    description: 'text',
+  },
+  {
+    id: 5,
+    title: 'Little cat',
+    img: 'https://astrafarm.com/images/encyclopedia/ittenVes170221.jpg',
+    description: 'text',
+  },
+  {
+    id: 6,
+    title: 'Big dog',
+    img: 'https://zoomagazin.dp.ua/image/catalog/blog-img/ponos.jpg',
+    description: 'text',
+  },
+  {
+    id: 7,
+    title: 'Little dog',
+    img: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/475B/production/_98776281_gettyimages-521697453.jpg',
+    description: 'text',
+  },
+];
+
+export default cards;
