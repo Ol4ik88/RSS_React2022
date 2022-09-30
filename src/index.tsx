68d947bde5cee0c8c05f820081172e23987f7359
Ol4ik88 <slapikol4ik@gmail.com>
2022-09-30 19:25:30 +0300

    refactor: add script for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/ol4ik88-REACT2022Q3">
       <App />
     </Router>
   </React.StrictMode>
