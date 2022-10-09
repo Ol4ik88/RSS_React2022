d3fab0475dabd05413b4c692caf4a1c157551614
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-09 17:15:31 +0300

    feat: implement page component with form

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,11 +1,32 @@
+import CardList from 'components/CardList/CardList';
+import Form from 'components/Form/Form';
 import React from 'react';
+import { ICard, IStateCards, PropsComponent } from 'type/type';
 
-function FormPage() {
-  return (
-    <section className="Form container" data-testid="FormPage">
-      <h2> It&apos;s FormPage</h2>
-    </section>
-  );
+class FormPage extends React.Component<PropsComponent, IStateCards> {
+  constructor(props: PropsComponent) {
+    super(props);
+    this.state = {
+      cards: [] as ICard[],
+    };
+  }
+
+  addCard = (card: ICard) => {
+    this.setState(({ cards }) => {
+      const listOldCards = [...cards];
+      return { cards: [...listOldCards, card] };
+    });
+  };
+
+  render() {
+    return (
+      <section className="form-page container" data-testid="FormPage">
+        <h2> It&apos;s FormPage</h2>
+        <Form addCard={this.addCard} />
+        <CardList cards={this.state.cards} />
+      </section>
+    );
+  }
 }
 
 export default FormPage;
