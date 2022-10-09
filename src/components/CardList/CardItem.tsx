806a2b13922d988f0b08073ff1a72a084cdd8add
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-09 16:32:21 +0300

    refactor: change rcard rendering

--- src/components/CardList/CardItem.tsx
+++ src/components/CardList/CardItem.tsx
@@ -5,9 +5,12 @@ import './CardList.scss';
 function CardItem(props: ICardItem) {
   return (
     <div className="card" data-testid="card">
-      <img src={props.card.img} className="foto" alt="foto" />
-      <h2>{props.card.title}</h2>
-      <p>{props.card.description}</p>
+      <img src={props.card.img as string} className="foto" alt="foto" />
+      <h2>{props.card.title || props.card.name}</h2>
+      {props.card.birthday && <div>birthday: {props.card.birthday}</div>}
+      {props.card.kind && <div>kind: {props.card.kind}</div>}
+      {props.card.sex && <div>sex: {props.card.sex}</div>}
+      {props.card.description && <div>description: {props.card.description}</div>}
     </div>
   );
 }
