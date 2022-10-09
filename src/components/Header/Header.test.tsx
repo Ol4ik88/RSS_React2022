d2f6f9f393224f610c569e07e7171991efa38569
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-09 16:46:51 +0300

    refactor: add tests for new components

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,12 +3,13 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../../App';
 import userEvent from '@testing-library/user-event';
+import Header from './Header';
 
 describe('Renders header-component', () => {
   test('renders header', () => {
     render(
       <Router>
-        <App />
+        <Header />
       </Router>
     );
     const headerMenu = screen.getByRole('banner');
@@ -21,23 +22,42 @@ describe('Renders header-component', () => {
       </Router>
     );
     const links = screen.getAllByRole('link');
-    expect(links).toHaveLength(2);
+    expect(links).toHaveLength(3);
   });
-  test('renders current page name', () => {
+  test('renders current home name', () => {
     render(
       <Router>
         <App />
       </Router>
     );
     const homePageLink = screen.getByTestId('home-link');
-    const aboutPageLink = screen.getByTestId('about-link');
 
     userEvent.click(homePageLink);
     const curPageHome = screen.getByText('Home page');
     expect(curPageHome).toBeInTheDocument();
+  });
+  test('renders current about name', () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const aboutPageLink = screen.getByTestId('about-link');
 
     userEvent.click(aboutPageLink);
     const curPageAbout = screen.getByText('About page');
     expect(curPageAbout).toBeInTheDocument();
+  });
+  test('renders current form name', () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const formPageLink = screen.getByTestId('form-link');
+
+    userEvent.click(formPageLink);
+    const curPageHome = screen.getByText('Form page');
+    expect(curPageHome).toBeInTheDocument();
   });
 });
