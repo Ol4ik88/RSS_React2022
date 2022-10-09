a005f8688ee1495af19aad002dc456c10eb0c705
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-09 16:35:59 +0300

    refactor: change code

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,21 +1,17 @@
-import cards from 'data/data';
 import React from 'react';
-import { ICards } from 'type/type';
+import { IStateCards } from 'type/type';
 import CardItem from './CardItem';
 import './CardList.scss';
 
-class CardList extends React.Component<ICards> {
-  constructor(props: ICards) {
+class CardList extends React.Component<IStateCards> {
+  constructor(props: IStateCards) {
     super(props);
-    this.state = {
-      cards: cards,
-    };
   }
   render() {
     return (
       <section className="cards" data-testid="card-list">
-        {this.props.cards.map((card) => {
-          return <CardItem card={card} key={card.id} />;
+        {this.props.cards.map((card, i) => {
+          return <CardItem card={card} key={card.id || i} />;
         })}
       </section>
     );
