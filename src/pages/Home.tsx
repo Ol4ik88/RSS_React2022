95fddcff3561192c10fff8fbcd65b89acdf5a4e1
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-16 13:10:48 +0300

    feat: change the implementation of the page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,97 @@
-import CardList from '../components/CardList/CardList';
-import Search from '../components/Search/Search';
+import Search from '../components/common/Search/Search';
 import React from 'react';
-import cards from '../data/data';
-import { IStateCards, PropsComponent } from 'type/type';
+import { ICharacter, IStateCardsAPI, PropsComponent } from 'type/type';
+import ListCharacter from 'components/ListCharacter/ListCharacter';
+import './Home.scss';
+import Loading from 'components/common/Loading/Loading';
+import Modal from 'components/common/Modal/Modal';
 
-class Home extends React.Component<PropsComponent, IStateCards> {
+const HOST = 'https://the-one-api.dev/v2/';
+const TOKEN = 'utxRxt1T7kr6gmJDi5LI';
+
+class Home extends React.Component<PropsComponent, IStateCardsAPI> {
   constructor(props: PropsComponent) {
     super(props);
     this.state = {
-      cards: cards,
+      cards: [],
+      isLoaded: false,
+      showCard: {} as ICharacter,
+      activeModal: false,
     };
     this.filterList = this.filterList.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
-  filterList(text: string) {
-    const filteredList =
-      cards.filter((card) => card.title.toLowerCase().includes(text.toLowerCase())) || cards;
-    this.setState({ cards: filteredList });
+
+  closeModal = () => {
+    this.setState({ activeModal: false });
+  };
+  showModal = (content: ICharacter) => {
+    this.setState({ showCard: content, activeModal: true });
+  };
+
+  async filterList(text: string) {
+    this.setState({ isLoaded: false });
+    const userText = text.trim()[0].toLocaleUpperCase() + text.trim().slice(1).toLocaleLowerCase();
+
+    try {
+      const response = await fetch(`${HOST}character?race=${userText}&limit=100`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          Accept: 'application/json',
+        },
+      });
+      const data = await response.json();
+
+      this.setState({ cards: data.docs, isLoaded: true });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(`${HOST}character?limit=100`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          Accept: 'application/json',
+        },
+      });
+      const data = await response.json();
+
+      this.setState({ cards: data.docs, isLoaded: true });
+    } catch (error) {
+      console.log(error);
+    }
   }
   render() {
+    const { cards, isLoaded, activeModal, showCard } = this.state;
     return (
       <section className="home container" data-testid="HomePage">
         <h2>It&apos;s homepage</h2>
-        <Search filter={this.filterList} />
-        <CardList cards={this.state.cards} />
+        {
+          <Search
+            filter={this.filterList}
+            label="Input example race characte: Elf, Human, Hobbit, Orc, Dwarf, Orc"
+          />
+        }
+        <>
+          {!isLoaded ? (
+            <div>
+              <Loading />
+            </div>
+          ) : cards.length !== 0 ? (
+            <>
+              <h2>Character Information about The Lord of the Rings</h2>
+              <ListCharacter cards={cards} onShowModal={this.showModal} />
+            </>
+          ) : (
+            <div>no data found</div>
+          )}
+        </>
+        <Modal card={showCard} activeModal={activeModal} onClose={this.closeModal} />
       </section>
     );
   }
