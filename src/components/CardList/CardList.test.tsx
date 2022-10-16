a681a0f3fbbefc8683f531708af1c37784bdc5cd
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-16 13:13:22 +0300

    refactor: change the test for the component

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages/Home';
-import cards from '../../data/data';
+import { cards } from '../../data/data';
 import CardList from './CardList';
 
 describe('<CardList />', () => {
-  test('renders card list', () => {
-    render(<Home />);
-    const cardList = screen.getByTestId('card-list');
-    expect(cardList).toBeInTheDocument();
-  });
   test('renders all cards', () => {
     render(<CardList cards={cards} />);
     const cardList = screen.getAllByTestId('card');
