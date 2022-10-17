33e1d2bfe81b6cab92ff8d572fafd3f0d7254b95
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-18 01:06:17 +0300

    refactor: replace class with functional component

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,47 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
-class Header extends React.Component {
-  state = {
-    currentPage: 'Home page',
-  };
-  getNamePage(namePage: string) {
-    this.setState({ ...this.state, currentPage: namePage });
-  }
-  render() {
-    return (
-      <header className="header container">
-        <h1>{this.state.currentPage}</h1>
-        <NavLink
-          end
-          to="/"
-          className="link"
-          data-testid="home-link"
-          onClick={() => this.getNamePage('Home page')}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/about"
-          className="link"
-          data-testid="about-link"
-          onClick={() => this.getNamePage('About page')}
-        >
-          About
-        </NavLink>
-        <NavLink
-          end
-          to="/form"
-          className="link"
-          data-testid="form-link"
-          onClick={() => this.getNamePage('Form page')}
-        >
-          Form
-        </NavLink>
-      </header>
-    );
-  }
+function Header() {
+  const [currentPage, setCurrentPage] = useState('Home page');
+  return (
+    <header className="header container">
+      <h1>{currentPage}</h1>
+      <NavLink end to="/" className="link" onClick={() => setCurrentPage('Home page')}>
+        Home
+      </NavLink>
+      <NavLink to="/about" className="link" onClick={() => setCurrentPage('About page')}>
+        About
+      </NavLink>
+      <NavLink end to="/form" className="link" onClick={() => setCurrentPage('Form page')}>
+        Form
+      </NavLink>
+    </header>
+  );
 }
 
 export default Header;
