b9905741dba2d19ed71a580ea3c7e5e05950332c
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-18 01:11:05 +0300

    refactor: replace class with functional component

--- src/components/ListCharacter/CardCharacter/CardCharacter.tsx
+++ src/components/ListCharacter/CardCharacter/CardCharacter.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import '../../CardList/CardList.scss';
 import { ICardCharacter } from './CardCharacter.type';
 
-class CardCharacter extends React.Component<ICardCharacter> {
-  render() {
-    const { card, onShowModal } = this.props;
-    return (
-      <>
-        <div
-          className="card"
-          data-testid="CardCharacter"
-          onClick={() => {
-            onShowModal(card);
-          }}
-        >
-          <h2>{card.name}</h2>
-          {card.race && <div>race: {card.race}</div>}
-          {card.gender && <div>gender: {card.gender}</div>}
-        </div>
-      </>
-    );
-  }
+function CardCharacter(props: ICardCharacter) {
+  const { card, onShowModal } = props;
+  return (
+    <>
+      <div
+        className="card"
+        data-testid="CardCharacter"
+        onClick={() => {
+          onShowModal(card);
+        }}
+      >
+        <h2>{card.name}</h2>
+        {card.race && <div>race: {card.race}</div>}
+        {card.gender && <div>gender: {card.gender}</div>}
+      </div>
+    </>
+  );
 }
 
 export default CardCharacter;
--- src/components/ListCharacter/ListCharacter.tsx
+++ src/components/ListCharacter/ListCharacter.tsx
@@ -3,16 +3,14 @@ import CardCharacter from './CardCharacter/CardCharacter';
 import '../CardList/CardList.scss';
 import { IListCharacter } from './ListCharacter.type';
 
-class ListCharacter extends React.Component<IListCharacter> {
-  render() {
-    return (
-      <section className="cards" data-testid="ListCharacter">
-        {this.props.cards.map((card) => {
-          return <CardCharacter card={card} key={card._id} onShowModal={this.props.onShowModal} />;
-        })}
-      </section>
-    );
-  }
+function ListCharacter(props: IListCharacter) {
+  return (
+    <section className="cards" data-testid="ListCharacter">
+      {props.cards.map((card) => {
+        return <CardCharacter card={card} key={card._id} onShowModal={props.onShowModal} />;
+      })}
+    </section>
+  );
 }
 
 export default ListCharacter;
