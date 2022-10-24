f7443b81723a6e0c9805ab154cbcb661cb66d46d
Ol4ik88 <slapikol4ik@gmail.com>
2022-10-24 15:11:47 +0300

    refactor: rewrite classes into functional components

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,32 +1,22 @@
 import CardList from 'components/CardList/CardList';
 import Form from 'components/Form/Form';
-import React from 'react';
-import { ICard, IStateCards, PropsComponent } from 'type/type';
+import React, { useState } from 'react';
+import { ICard } from 'type/type';
 
-class FormPage extends React.Component<PropsComponent, IStateCards> {
-  constructor(props: PropsComponent) {
-    super(props);
-    this.state = {
-      cards: [] as ICard[],
-    };
-  }
+function FormPage() {
+  const [cards, setCards] = useState<ICard[]>([]);
 
-  addCard = (card: ICard) => {
-    this.setState(({ cards }) => {
-      const listOldCards = [...cards];
-      return { cards: [...listOldCards, card] };
-    });
+  const addCard = (card: ICard) => {
+    setCards([...cards, card]);
   };
 
-  render() {
-    return (
-      <section className="form-page container">
-        <h2> It&apos;s FormPage</h2>
-        <Form addCard={this.addCard} />
-        <CardList cards={this.state.cards} />
-      </section>
-    );
-  }
+  return (
+    <section className="form-page container">
+      <h2> It&apos;s FormPage</h2>
+      <Form addCard={addCard} />
+      <CardList cards={cards} />
+    </section>
+  );
 }
 
 export default FormPage;
