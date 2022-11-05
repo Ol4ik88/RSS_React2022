bde92f827ee87060d11270130d0ee3925b94afe6
Ol4ik88 <slapikol4ik@gmail.com>
2022-11-05 18:12:10 +0300

    refactor: change configureStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,7 @@ export const store = configureStore({
     home: homeReducer,
     form: formReducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type AppDispatch = typeof store.dispatch;
